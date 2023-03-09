00e7a24d5f684a54ccfee08582d88b27dd9b36a9
Kevin Martens <kevin_m@gmx.de>
2023-03-09 10:49:02 +0100

    last fixes, we have a working make_dataframe_row function

--- python_files/ml_logic/feature_dataframe.py
+++ python_files/ml_logic/feature_dataframe.py
@@ -3,26 +3,26 @@ from python_files.feature_engineering import feature_functions as ff
 
 def make_dataframe_row(home, away, matchday, df, past_matchdays = 5):
     dicto = {}
-    dicto = {'home':home}
-    dicto = {'away':away}
-    dicto = {'date': matchday}
-    dicto = {'shots_h':ff.get_shots(home, matchday, df, past_matchdays)}
-    dicto = {'targets_h':ff.get_targets(home, matchday, df, past_matchdays)}
-    dicto = {'goals_h':ff.get_goals(home, matchday, df, past_matchdays)}
-    dicto = {'conc_h':ff.get_conc(home, matchday, df, past_matchdays)}
-    dicto = {'corner_h':ff.get_corner(home, matchday, df, past_matchdays)}
-    dicto = {'goaldiff_h':ff.get_goal_diff(home, matchday, df)}
-    dicto = {'opp_avg_h':ff.get_opp_avg(home, matchday, df, past_matchdays)}
-    dicto = {'value_h':ff.get_squad_value(home)}
-    dicto = {'targets_a':ff.get_targets(away, matchday, df, past_matchdays)}
-    dicto = {'shots_a':ff.get_shots(away, matchday, df, past_matchdays)}
-    dicto = {'goals_a':ff.get_goals(away, matchday, df, past_matchdays)}
-    dicto = {'conc_a':ff.get_conc(away, matchday, df, past_matchdays)}
-    dicto = {'corner_a':ff.get_corner(away, matchday, df, past_matchdays)}
-    dicto = {'goaldiff_a':ff.get_goal_diff(home, matchday, df)}
-    dicto = {'opp_avg_a':ff.get_opp_avg(away, matchday, df, past_matchdays)}
-    dicto = {'value_a':ff.get_squad_value(away)}
-    dicto = {'win_home':ff.get_win_home(home, away, matchday, df)}
-    dicto = {'draw':ff.get_draw(home, away, matchday, df)}
-    dicto = {'win_away':ff.get_win_away(home, away, matchday, df)}
-    return pd.DataFrame.from_dict(dicto)
+    dicto = {'home':home,
+             'away':away,
+             'date': matchday,
+             'shots_h':ff.get_shots(home, matchday, df, past_matchdays),
+             'targets_h':ff.get_targets(home, matchday, df, past_matchdays),
+             'goals_h':ff.get_goals(home, matchday, df, past_matchdays),
+             'conc_h':ff.get_conc(home, matchday, df, past_matchdays),
+             'corner_h':ff.get_corner(home, matchday, df, past_matchdays),
+             'goaldiff_h':ff.get_goal_diff(home, matchday, df),
+             'opp_avg_h':ff.get_opp_avg(home, matchday, df, past_matchdays),
+             'value_h':ff.get_squad_value(home),
+             'targets_a':ff.get_targets(away, matchday, df, past_matchdays),
+             'shots_a':ff.get_shots(away, matchday, df, past_matchdays),
+             'goals_a':ff.get_goals(away, matchday, df, past_matchdays),
+             'conc_a':ff.get_conc(away, matchday, df, past_matchdays),
+             'corner_a':ff.get_corner(away, matchday, df, past_matchdays),
+             'goaldiff_a':ff.get_goal_diff(home, matchday, df),
+             'opp_avg_a':ff.get_opp_avg(away, matchday, df, past_matchdays),
+             'value_a':ff.get_squad_value(away),
+             'win_home':ff.get_win_home(home, away, matchday, df),
+             'draw':ff.get_draw(home, away, matchday, df),
+             'win_away':ff.get_win_away(home, away, matchday, df)}
+    return pd.DataFrame(dicto, index=[0])
