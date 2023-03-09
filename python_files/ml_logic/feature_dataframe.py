04d08a6661d74cb0f9f869b2498ca0fe7ec01898
Kevin Martens <kevin_m@gmx.de>
2023-03-09 11:45:50 +0100

    changed squad values unit to millions of euros

--- python_files/ml_logic/feature_dataframe.py
+++ python_files/ml_logic/feature_dataframe.py
@@ -13,7 +13,7 @@ def make_dataframe_row(home, away, matchday, df, past_matchdays = 5):
              'corner_h':ff.get_corner(home, matchday, df, past_matchdays),
              'goaldiff_h':ff.get_goal_diff(home, matchday, df),
              'opp_avg_h':ff.get_opp_avg(home, matchday, df, past_matchdays),
-             'value_h':ff.get_squad_value(home),
+             'value_h [M]':(ff.get_squad_value(home)/1000000),
              'targets_a':ff.get_targets(away, matchday, df, past_matchdays),
              'shots_a':ff.get_shots(away, matchday, df, past_matchdays),
              'goals_a':ff.get_goals(away, matchday, df, past_matchdays),
@@ -21,7 +21,7 @@ def make_dataframe_row(home, away, matchday, df, past_matchdays = 5):
              'corner_a':ff.get_corner(away, matchday, df, past_matchdays),
              'goaldiff_a':ff.get_goal_diff(home, matchday, df),
              'opp_avg_a':ff.get_opp_avg(away, matchday, df, past_matchdays),
-             'value_a':ff.get_squad_value(away),
+             'value_a [M]':(ff.get_squad_value(away)/1000000),
              'win_home':ff.get_win_home(home, away, matchday, df),
              'draw':ff.get_draw(home, away, matchday, df),
              'win_away':ff.get_win_away(home, away, matchday, df)}
