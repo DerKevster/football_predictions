adec6b967519c0f723e7499c75cc01c0fbd72037
pauleisenhut <paul_eisenhut@me.com>
2023-03-09 15:35:33 +0100

    added the make_feature_df function

--- python_files/ml_logic/feature_dataframe.py
+++ python_files/ml_logic/feature_dataframe.py
@@ -36,4 +36,5 @@ def make_feature_df(df, past_matchdays):
         new_df = make_dataframe_row(df.at[index, "HomeTeam"], df.at[index, "away_team"], df.at[index,"matchday"], df)
         feature_df = pd.concat([feature_df, pd.DataFrame(new_df)], axis=0)
     feature_df = feature_df.reset_index()
+    feature_df.drop(columns=["index"])
     return feature_df
