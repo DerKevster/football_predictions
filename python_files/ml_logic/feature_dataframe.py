8b7241377f6310a84b10edbe0f4d6d3a31661126
Claudio Pizarro Salcedo <claudito48@hotmail.de>
2023-03-13 12:25:15 +0100

    learned not to push to master pt.2

--- python_files/ml_logic/feature_dataframe.py
+++ python_files/ml_logic/feature_dataframe.py
@@ -6,31 +6,31 @@ from python_files.feature_engineering.merge_dataframes import make_merged_df, ma
 # Leagues: ['PL', 'BL', 'LL', 'SA']
 # Seasons: ['18-19', '19-20', '20-21', '21-22', '22-23']
 
-def make_dataframe_row(home, away, matchday, merged_df, fifa_df, squad_value_df, past_matchdays = 5):
+def make_dataframe_row(home, away, matchday, merged_df, fifa_df, squad_value_df, past_matches = 5):
 
     dicto = {}
     dicto = {'home':home,
              'away':away,
              'matchday' : matchday,
-             'shots_h' : ff.get_shots(home, matchday, merged_df, past_matchdays),
-             'shots_ot_h' : ff.get_shots_ot(home, matchday, merged_df, past_matchdays),
-             'goals_h' : ff.get_goals(home, matchday, merged_df, past_matchdays),
-             'conc_h' : ff.get_conc(home, matchday, merged_df, past_matchdays),
-             'corner_h' : ff.get_corner(home, matchday, merged_df, past_matchdays),
+             'shots_h' : ff.get_shots(home, matchday, merged_df, past_matches),
+             'shots_ot_h' : ff.get_shots_ot(home, matchday, merged_df, past_matches),
+             'goals_h' : ff.get_goals(home, matchday, merged_df, past_matches),
+             'conc_h' : ff.get_conc(home, matchday, merged_df, past_matches),
+             'corner_h' : ff.get_corners(home, matchday, merged_df, past_matches),
              'goaldiff_h' : ff.get_goal_diff(home, matchday, merged_df),
-             'opp_avg_h' : ff.get_opp_avg(home, matchday, merged_df, past_matchdays),
+             'opp_avg_h' : ff.get_opp_avg(home, matchday, merged_df, past_matches),
              'value_h' : ff.get_squad_value(home, squad_value_df),
              'attack_h' : ff.get_attack(home, fifa_df),
              'midfield_h' : ff.get_midfield(home, fifa_df),
              'defense_h' : ff.get_defense(home, fifa_df),
              'bench_h' : ff.get_bench(home, fifa_df),
-             'shots_ot_a' : ff.get_shots_ot(away, matchday, merged_df, past_matchdays),
-             'shots_a' : ff.get_shots(away, matchday, merged_df, past_matchdays),
-             'goals_a' : ff.get_goals(away, matchday, merged_df, past_matchdays),
-             'conc_a' : ff.get_conc(away, matchday, merged_df, past_matchdays),
-             'corner_a' : ff.get_corner(away, matchday, merged_df, past_matchdays),
+             'shots_ot_a' : ff.get_shots_ot(away, matchday, merged_df, past_matches),
+             'shots_a' : ff.get_shots(away, matchday, merged_df, past_matches),
+             'goals_a' : ff.get_goals(away, matchday, merged_df, past_matches),
+             'conc_a' : ff.get_conc(away, matchday, merged_df, past_matches),
+             'corner_a' : ff.get_corners(away, matchday, merged_df, past_matches),
              'goaldiff_a' : ff.get_goal_diff(away, matchday, merged_df),
-             'opp_avg_a' : ff.get_opp_avg(away, matchday, merged_df, past_matchdays),
+             'opp_avg_a' : ff.get_opp_avg(away, matchday, merged_df, past_matches),
              'value_a' : ff.get_squad_value(away, squad_value_df),
              'attack_a' : ff.get_attack(away, fifa_df),
              'midfield_a' : ff.get_midfield(away, fifa_df),
@@ -41,17 +41,17 @@ def make_dataframe_row(home, away, matchday, merged_df, fifa_df, squad_value_df,
     return pd.DataFrame(dicto, index=[0])
 
 
-def make_feature_df(league, season, past_matchdays):
+def make_feature_df(league, season, past_matches):
 
     merged_df = make_merged_df(league, season)
     fifa_df = make_fifa_df(season)
     squad_value_df = make_squad_value_df(season)
 
-    index_match = merged_df.index[merged_df['matchday']==(past_matchdays+1)].tolist()[0]
+    index_match = merged_df.index[merged_df['matchday']==(past_matches+1)].tolist()[0]
     feature_df = pd.DataFrame()
 
     for index, matchday in merged_df.loc[index_match : , : ].iterrows():
-        new_df = make_dataframe_row(merged_df.at[index, "HomeTeam"], merged_df.at[index, "away_team"], merged_df.at[index,"matchday"], merged_df, fifa_df, squad_value_df, past_matchdays=5)
+        new_df = make_dataframe_row(merged_df.at[index, "HomeTeam"], merged_df.at[index, "away_team"], merged_df.at[index,"matchday"], merged_df, fifa_df, squad_value_df, past_matches=5)
         feature_df = pd.concat([feature_df, pd.DataFrame(new_df)], axis=0)
     feature_df = feature_df.reset_index()
     feature_df = feature_df.drop(columns=["index"])
