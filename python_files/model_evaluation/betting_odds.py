45bd69d6782ca2c79d8c6c7d4597501d87586c5a
Claudio Pizarro Salcedo <claudito48@hotmail.de>
2023-03-14 11:47:13 +0100

    added proba input to make_betting_odds

--- python_files/model_evaluation/betting_odds.py
+++ python_files/model_evaluation/betting_odds.py
@@ -3,9 +3,9 @@ import pandas as pd
 from python_files.feature_engineering.merge_dataframes import make_footballdatauk_DataFrames
 
 # Function to generate df to show bet365 and pinnacle betting odds along date, teams, and result
-def make_odds_df(league, season):
+def make_betting_odds_df(league, season, proba=False):
     # Extract features
-    odds_df = make_footballdatauk_DataFrames()
+    betting_odds_df = make_footballdatauk_DataFrames()
 
     # Translate league names
     league_translator = {
@@ -16,16 +16,23 @@ def make_odds_df(league, season):
     }
 
     league_translated = league_translator[league]
-    odds_df = odds_df[f'{league_translated}_{season}.csv']
+    betting_odds_df = betting_odds_df[f'{league_translated}_{season}.csv']
 
     # Strip whitespace from column names
-    odds_df.columns = odds_df.columns.str.strip()
+    betting_odds_df.columns = betting_odds_df.columns.str.strip()
 
     # Sort by dates as datetime objects
-    odds_df['Date'] = pd.to_datetime(odds_df['Date'], dayfirst=True)
-    odds_df.sort_values(by='Date', ascending=True)
+    betting_odds_df['Date'] = pd.to_datetime(betting_odds_df['Date'], dayfirst=True)
+    betting_odds_df.sort_values(by='Date', ascending=True)
 
     # Get follwing Columns: 'Date', 'HomeTeam', 'away_team', 'B365H', 'B365D', 'B365A', 'PSH', 'PSD', 'PSA'
-    odds_df = odds_df[['Date', 'HomeTeam', 'AwayTeam', 'B365H', 'B365D', 'B365A', 'PSH', 'PSD', 'PSA', 'FTR']]
-    return odds_df
-    # To convert odds to percentage: decimal percentage = (1 / odds) * 100
+    betting_odds_df = betting_odds_df[['Date', 'HomeTeam', 'AwayTeam', 'B365H', 'B365D', 'B365A', 'PSH', 'PSD', 'PSA', 'FTR']]
+
+    if proba:
+        columns = ['B365H', 'B365D', 'B365A', 'PSH', 'PSD', 'PSA']
+
+        for col in columns:
+            betting_odds_df[col] = (1 / betting_odds_df[col])
+
+    return betting_odds_df
+    # To convert odds to percentage: decimal_percentage = (1 / odds)
