774f101ab59a4f9d39f7b6ace9d823d27859d01a
Christoph Baunach <cbaunach@gmail.com>
2023-03-15 15:19:00 +0100

    update model_functions

--- python_files/model_evaluation/model_functions.py
+++ python_files/model_evaluation/model_functions.py
@@ -6,32 +6,73 @@ from sklearn.metrics import accuracy_score, f1_score, precision_score, classific
 from sklearn.preprocessing import StandardScaler
 
 
-def make_predict_df(df, model):
-
-    #Create X and y
+def make_X_y(df):
+     #Create X and y
     X = df.drop(columns=['home','away','date','outcome'],axis=1)
     y = df['outcome']
     # Split df into train, test set
     X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size = 0.2, random_state = 42)
+    return X_train, X_test, y_train, y_test
 
-    # Scale df
-    scaler = StandardScaler()
-    X_train_scaled = scaler.fit_transform(X_train)
-    X_test_scaled= scaler.transform(X_test)
 
-    # optimized model
-    mod = model
-    # fit model
-    mod.fit(X_train_scaled, y_train)
-    # predict_probability
-    proba = mod.predict_proba(X_test_scaled)
+def make_predict_df(df, model, split=True, chrono=False):
 
-    df_prob = pd.DataFrame()
-    df_prob[['home','away']] = df.loc[y_test.index, ['home','away']]
-    df_prob['outcome'] = y_test
-    df_prob['home_win'] = proba[:,0]
-    df_prob['draw'] = proba[:,1]
-    df_prob['away_win'] = proba[:,2]
+    if split:
+        #Create X and y
+        X = df.drop(columns=['home','away','date','outcome'],axis=1)
+        y = df['outcome']
+
+        if not chrono:
+            # Split df into train, test set
+            X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size = 0.2, random_state = 42)
+        else:
+            X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size = 0.2, shuffle = False)
+
+        # Scale df
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled= scaler.transform(X_test)
+
+        # optimized model
+        mod = model
+        # fit model
+        mod.fit(X_train_scaled, y_train)
+        # predict_probability
+        proba = mod.predict_proba(X_test_scaled)
+
+        df_prob = pd.DataFrame()
+        df_prob[['Date']] = df.loc[y_test.index, ['date']]
+        df_prob[['HomeTeam','AwayTeam']] = df.loc[y_test.index, ['home','away']]
+        df_prob['outcome'] = y_test
+        df_prob['home_win'] = proba[:,0]
+        df_prob['draw'] = proba[:,1]
+        df_prob['away_win'] = proba[:,2]
+
+    else:
+        #Create X and y
+        X = df.drop(columns=['home','away','date','outcome'],axis=1)
+        y = df['outcome']
+
+        # Scale df
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X)
+
+        # optimized model
+        mod = model
+        # fit model
+        mod.fit(X_scaled, y)
+        # predict_probability
+        proba = mod.predict_proba(X_scaled)
+
+        df_prob = pd.DataFrame()
+        df_prob[['Date']] = df.loc[:, ['date']]
+        df_prob[['HomeTeam','AwayTeam']] = df.loc[:, ['home','away']]
+        df_prob['outcome'] = y
+        df_prob['home_win'] = proba[:,0]
+        df_prob['draw'] = proba[:,1]
+        df_prob['away_win'] = proba[:,2]
 
     return df_prob
