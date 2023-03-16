ad45a2eeaca5771372c5c3f891520563a08b472f
Claudio Pizarro Salcedo <claudito48@hotmail.de>
2023-03-16 16:45:57 +0100

    fixed anke preds with split and make predict df (big shoutout to ibrahim aka code jesus)

--- python_files/model_evaluation/model_functions.py
+++ python_files/model_evaluation/model_functions.py
@@ -12,91 +12,53 @@ def make_X_y_split(X_df, y_df, p, n):
     X_train = X_df.iloc[p: p + n, :]
     y_train = y_df.iloc[p: p + n]
 
-    X_test = X_df.iloc[p + n, :]
+    X_test = pd.DataFrame(X_df.iloc[p + n, :]).T
     y_test = y_df.iloc[p + n]
     return X_train, X_test, y_train, y_test
 
 
 # Function to split the data in chronological order (i.e take n previous games and predict the following one, then iterate through data in steps of p)
-def make_preds_with_split(feature_df, model, n=5, p_increment=1):
+def make_preds_with_split(feature_df, model, n=15, p_increment=1):
     p = 0
     pred_list = []
-    match_outcomes = []
-    pipe = make_pipeline(StandardScaler(), model)
+    scaler = StandardScaler()
 
-    while p <= len(feature_df-(n+1)):
+    while p <= len(feature_df)-(n+1):
       #Create X and y
+      merge_info_df = feature_df[['home','away','date','outcome']].iloc[n:, :]
       X_df = feature_df.drop(columns=['home','away','date','outcome'],axis=1)
       y_df = feature_df['outcome']
 
       X_train, X_test, y_train, y_test = make_X_y_split(X_df, y_df, p, n)
+      X_train = scaler.fit_transform(X_train)
+      X_test = scaler.transform(X_test)
+      model = model.fit(X_train, y_train)
+      res_preds = list(model.predict_proba(X_test)[0])
+      preds_dict = {key: value for key, value in zip(y_train.value_counts().index, res_preds)}
+      if len(y_train.value_counts()) == 2:
+          preds_dict[list({0, 1, 2} - set(y_train.value_counts().index))[0]] = 0
 
-      pipe.fit(X_train, y_train)
-      res_preds = pipe.predict_proba(X_test)
-      pred_list.append(res_preds)
-      match_outcomes.append(y_test)
+      pred_list.append(preds_dict)
       p += p_increment
+    return pred_list, merge_info_df
 
-    return pred_list, match_outcomes
 
-
-def make_predict_df(df, model, split=True, chrono=False, random_state=42):
-
-    if split:
-        #Create X and y
-        X = df.drop(columns=['home','away','date','outcome'],axis=1)
-        y = df['outcome']
-
-        if not chrono:
-            # Split df into train, test set
-            X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size = 0.2, random_state = random_state)
-        else:
-            X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size = 0.2, shuffle = False)
-
-        # Scale df
-        scaler = StandardScaler()
-        X_train_scaled = scaler.fit_transform(X_train)
-        X_test_scaled= scaler.transform(X_test)
-
-        # optimized model
-        mod = model
-        # fit model
-        mod.fit(X_train_scaled, y_train)
-        # predict_probability
-        proba = mod.predict_proba(X_test_scaled)
-
-        df_prob = pd.DataFrame()
-        df_prob[['Date']] = df.loc[y_test.index, ['date']]
-        df_prob[['HomeTeam','AwayTeam']] = df.loc[y_test.index, ['home','away']]
-        df_prob['outcome'] = y_test
-        df_prob['home_win'] = proba[:,0]
-        df_prob['draw'] = proba[:,1]
-        df_prob['away_win'] = proba[:,2]
-
-    else:
-        #Create X and y
-        X = df.drop(columns=['home','away','date','outcome'],axis=1)
-        y = df['outcome']
-
-        # Scale df
-        scaler = StandardScaler()
-        X_scaled = scaler.fit_transform(X)
-
-        # optimized model
-        mod = model
-        # fit model
-        mod.fit(X_scaled, y)
-        # predict_probability
-        proba = mod.predict_proba(X_scaled)
-
-        df_prob = pd.DataFrame()
-        df_prob[['Date']] = df.loc[:, ['date']]
-        df_prob[['HomeTeam','AwayTeam']] = df.loc[:, ['home','away']]
-        df_prob['outcome'] = y
-        df_prob['home_win'] = proba[:,0]
-        df_prob['draw'] = proba[:,1]
-        df_prob['away_win'] = proba[:,2]
+def make_predict_df(pred_list, merge_info_df):
+    df_prob = merge_info_df
+    df_prob['home_win'] = [pred[0] for pred in pred_list]
+    df_prob['draw']     = [pred[1] for pred in pred_list]
+    df_prob['away_win'] = [pred[2] for pred in pred_list]
 
     return df_prob
+
+
+
+if __name__ == '__main__':
+    from python_files.ml_logic.feature_dataframe import make_feature_df
+    from sklearn.svm import SVC
+    feature_df = make_feature_df('BL','18-19', 5)
+    svc = SVC(kernel= 'rbf', probability=True)
+    pred_list, merge_info_df = make_preds_with_split(feature_df, svc)
+
+    result1 = make_predict_df(pred_list, merge_info_df)
+    print(result1)
